5e831b7ed37748cf278bb36f1623bd9aabd61819
elton-souza <elton.souza1515@gmail.com>
2021-09-03 20:43:19 -0300

    trabalhando com componentes

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,16 +1,25 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
-    <ul>
-      <li v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo" />
+
+  <div class="corpo">
+    <h1 class="texto-centralizado">{{ titulo }}</h1>
+    <ul class="lista-fotos">
+       <li v-for="foto of fotos" :key="foto.id" class="lista-fotos-item">
+           <!-- tudo que estiver no componente 'meu-painel' será adicionado no slot -->
+         <meu-painel :titulo="foto.titulo"> 
+           <img :src="foto.url" :alt="foto.titulo" class="imagem-responsiva" />
+         </meu-painel>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+
+import Painel from './components/shared/painel/Painel.vue';
 export default {
+  components:{
+    'meu-painel': Painel
+  },
   data() {
     // Função padrão que retorna dados
     return {
@@ -26,6 +35,28 @@ export default {
       .then((dados) => (this.fotos = dados));
   },
 };
+
 </script>
 <style>
+
+  .corpo{
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
+
+  .texto-centralizado{
+    text-align: center;
+  }
+
+  .lista-fotos{
+    list-style: none;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .imagem-responsiva{
+    width: 100%;
+  }
+
 </style>
