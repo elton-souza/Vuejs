4655a984412fef82b5f5d2dd4f9347d37102a51d
Elton Souza <elton.souza1515@gmail.com>
2021-09-03 18:02:20 -0300

    iniciando vue

---
+++ alurapic/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <ul>
+      <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    // Função padrão que retorna dados
+    return {
+      titulo: "Alurapic",
+      fotos: [],
+    };
+  },
+  created() {
+    // Executa assim que o componente é criado || LifeCycle Hooks
+    this.$http // No proprio componente, acessamos a propriedade http
+      .get("http://localhost:3000/v1/fotos") // API
+      .then((response) => response.json())
+      .then((dados) => (this.fotos = dados));
+  },
+};
+</script>
+<style>
+</style>
